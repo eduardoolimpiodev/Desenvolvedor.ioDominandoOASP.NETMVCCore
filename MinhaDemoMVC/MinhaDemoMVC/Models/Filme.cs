93207a8497641dac28c82c6e0cc18678f2b8f703
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-04-11 19:54:33 -0300

    add: prop Id

--- MinhaDemoMVC/MinhaDemoMVC/Models/Filme.cs
+++ MinhaDemoMVC/MinhaDemoMVC/Models/Filme.cs
@@ -7,6 +7,7 @@ namespace MinhaDemoMVC.Models
 {
     public class Filme
     {
+        public int Id { get; set; }
         public string Titulo { get; set; }
         public DateTime DataLancamento { get; set; }
         public string Genero { get; set; }
