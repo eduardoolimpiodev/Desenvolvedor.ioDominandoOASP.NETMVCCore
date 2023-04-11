a7d377247c10aaf4d1768691748638f600739c13
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-04-11 19:15:39 -0300

    add: rotas específicas, inclusive na página de erro para nãohaver conflito entre as rotas.

--- MinhaDemoMVC/MinhaDemoMVC/Controllers/HomeController.cs
+++ MinhaDemoMVC/MinhaDemoMVC/Controllers/HomeController.cs
@@ -8,19 +8,26 @@ using System.Threading.Tasks;
 
 namespace MinhaDemoMVC.Controllers
 {
+    [Route("")]
+    [Route("gestao-clientes")]
     public class HomeController : Controller
     {
+        [Route("")]
+        [Route("pagina-inicial")]
         public IActionResult Index(string id, string categoria)
         {
             return View();
         }
 
+        [Route("privacidade")]
+        [Route("politica-de-privacidade")]
         public IActionResult Privacy()
         {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("erro-encontrado")]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
